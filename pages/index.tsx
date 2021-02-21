60a57ed7bb51af09cabd6013e20ca7737f02f3ee
karimLa <kira00luffy@gmail.com>
2021-02-21 12:57:19 +0100

    added background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import ComingSoon from '../components/ComingSoon';
 
 export default function Home() {
 	return (
-		<div className="h-screen">
+		<div className='h-screen bg-gray-100'>
 			<Head>
 				<title>Momento Shots</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className="h-full flex justify-center items-center">
+			<main className='h-full flex justify-center items-center'>
 				<ComingSoon />
 			</main>
 		</div>
