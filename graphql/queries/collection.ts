e483f10bfbb45af2f0412499f0fe0c7089585db6
KarimLa <karimacc001@gmail.com>
2021-04-11 17:26:48 +0000

    using generated Query type

--- graphql/queries/collection.ts
+++ graphql/queries/collection.ts
@@ -1,29 +1,29 @@
 import { gql } from '@apollo/client'
 
-import { Collection } from '../types'
-import { _ApolloClient } from '@/lib/apolloClient'
+import type { Query } from '../types'
+import type { _ApolloClient } from '@/lib/apolloClient'
 
 export const COLLECTIONS_QUERY = gql`
 	query {
-  collections {
-    id
-    name
-    items {
-      highlight
-      image {
-        id
-        url
-        alternativeText
-        width
-        height
+    collections {
+      id
+      name
+      items {
+        highlight
+        image {
+          id
+          url
+          alternativeText
+          width
+          height
+        }
       }
     }
   }
-}
 `
 
 export type CollectionsResponse = {
-  collections: Collection[]
+  collections: Query['collections']
 }
 
 export async function collectionFind(apolloClient: _ApolloClient) {
