7b0b94c92757631d3b0f79582762a7f965b383df
KarimLa <karimacc001@gmail.com>
2021-06-19 12:44:24 +0100

    about us section

--- components/shared/Thumbnail.tsx
+++ components/shared/Thumbnail.tsx
@@ -2,19 +2,29 @@ import Image, { ImageProps } from 'next/image';
 
 type Props = ImageProps & {
   overlay?: boolean;
+  boxPosition?: 'bl' | 'br';
 };
 
-const Thumbnail: React.VFC<Props> = ({ overlay, ...props }) => {
+const Thumbnail: React.VFC<Props> = ({
+  overlay,
+  boxPosition = 'bl',
+  ...props
+}) => {
   return (
-    <div className="h-full shadow-md relative">
+    <div className="shadow-lg relative">
       <div
-        className="w-full absolute -left-3 top-3 -bottom-3 right-0 border-2 border-gray-200"
+        className={`w-full border-2 border-gray-200 absolute -bottom-3 top-3 ${
+          boxPosition === 'bl' ? '-left-3' : '-right-3'
+        }`}
         aria-hidden
       />
       <Image {...props} alt={props.alt} />
 
       {overlay && (
-        <div className="absolute inset-0 bg-gradient-to-tl from-transparent to-gray-50 opacity-20" />
+        <div
+          className="absolute inset-0 bg-gradient-to-tl from-transparent to-gray-50 opacity-20"
+          aria-hidden
+        />
       )}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import Thumbnail from '@/components/shared/Thumbnail';
 import Head from 'next/head';
 
 import ownerPortrait from '@/images/photographer-1.jpg';
+import ownerLandscape from '@/images/photographer-2.jpg';
 
 const HomePage = () => {
   return (
-    <main>
+    <main className="space-y-52 md:space-y-64">
       <Head>
         <title>Momento Shots</title>
       </Head>
@@ -31,8 +32,33 @@ const HomePage = () => {
             layout="responsive"
             width="1920"
             height="2560"
-            overlay
             placeholder="blur"
+            overlay
+          />
+        </div>
+      </section>
+
+      <section className="space-y-8 md:space-y-0 md:flex">
+        <div className="px-4 md:px-0 md:order-2 md:w-2/5 md:-ml-24 md:mt-10 md:z-10 lg:mt-20">
+          <h2 className="inline-block px-6 py-4 text-3xl font-display text-white bg-secondary sm:text-4xl lg:text-5xl">
+            Who are we
+          </h2>
+          <p className="px-6 py-4 text-gray-200 bg-secondary lg:text-xl">
+            Outdoor Photographer based out of Marrakech, Morocco. Outdoor
+            Photographer based out of Marrakech, Morocco. Outdoor Photographer
+            based out of Marrakech, Morocco.
+          </p>
+        </div>
+        <div className="overflow-hidden md:w-3/5">
+          <Thumbnail
+            src={ownerLandscape}
+            alt="Oumaima Hoummir"
+            layout="responsive"
+            width="1920"
+            height="1080"
+            placeholder="blur"
+            objectFit="cover"
+            boxPosition="br"
           />
         </div>
       </section>
