52e4f26540471a75fe5c14c8f87db993c1ccc783
KarimLa <karimacc001@gmail.com>
2021-06-24 20:18:15 +0100

    added slug and description to collection listing

--- components/collections/Collection.tsx
+++ components/collections/Collection.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { pad } from '@/utils/index';
 import { ICollections } from '@/types/pages/collections';
-import Link from 'next/link';
+import IconExternalLink from '@/components/icons/ExternalLink';
 
 interface Props {
   collection: ICollections[0];
@@ -10,36 +11,49 @@ interface Props {
 }
 
 const Collection: React.VFC<Props> = ({ collection, index }) => {
-  const description =
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, fugit sunt blanditiis eveniet ullam reiciendis tenetur? Culpa tempore architecto cupiditate sunt aspernatur velit nemo hic dicta nulla quos. Odio, beatae?';
+  const link = `/collections/${collection.slug}`;
+  const linkLabel = `${collection.name} collection`;
 
   return (
-    <article className="overflow-hidden rounded-md shadow-md md:w-[48%] md:mr-[2%]">
-      <Link href="/">
-        <a className="flex">
-          <div className="w-1/2 h-60">
-            <Image
-              src={collection.headerImage.url}
-              alt={collection.headerImage.alternativeText}
-              width="400"
-              height="380"
-              layout="responsive"
-              objectFit="fill"
-            />
-          </div>
-          <div className="w-1/2 h-60 p-4">
-            <span className="text-7xl text-gray-200 lg:text-8xl">
-              #{pad(index, 2)}
-            </span>
-            <h2 className="-mt-5 text-2xl font-medium font-display text-gray-500 lg:text-3xl">
-              {collection.name}
-            </h2>
-            <p className="text-sm text-gray-400 lg:text-base">
-              {description.substring(0, 100)}...
-            </p>
-          </div>
-        </a>
-      </Link>
+    <article className="p-4 md:py-6 md:w-1/2">
+      <div className="overflow-hidden flex rounded-md shadow-md">
+        <div className="w-1/2 h-60">
+          <Link href={link}>
+            <a aria-label={linkLabel}>
+              <Image
+                src={collection.headerImage.url}
+                alt={collection.headerImage.alternativeText}
+                width="400"
+                height="380"
+                layout="responsive"
+              />
+            </a>
+          </Link>
+        </div>
+        <div className="w-1/2 h-60 p-4 relative">
+          <span className="inline-block -mt-4 text-8xl text-gray-200">
+            #{pad(index, 2)}
+          </span>
+          <h2
+            className="-mt-5 text-2xl font-medium font-display text-gray-500 truncate md:-mt-6 md:text-3xl lg:text-4xl"
+            title={collection.name}
+          >
+            {collection.name}
+          </h2>
+          <p className="mt-2 text-sm line-clamp-4 text-gray-400 lg:text-base lg:line-clamp-3">
+            {collection.description}
+          </p>
+          <Link href={link}>
+            <a
+              target="_blank"
+              className="absolute right-4 bottom-4"
+              aria-label={linkLabel}
+            >
+              <IconExternalLink />
+            </a>
+          </Link>
+        </div>
+      </div>
     </article>
   );
 };
--- components/collections/Collections.tsx
+++ components/collections/Collections.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 const Collections: React.VFC<Props> = ({ collections }) => {
   return (
-    <div className="space-y-8 md:space-y-0 md:flex md:flex-wrap md:justify-between">
+    <div className="md:flex md:flex-wrap md:justify-between">
       {collections.map((colection, i) => (
         <Collection collection={colection} index={i + 1} key={colection.id} />
       ))}
