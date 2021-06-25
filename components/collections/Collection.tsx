87dfe5904075abd506212c4d316e365093e615dc
KarimLa <karimacc001@gmail.com>
2021-06-25 12:08:44 +0100

    colllection layout with grid instead of flexbox

--- components/collections/Collection.tsx
+++ components/collections/Collection.tsx
@@ -15,43 +15,41 @@ const Collection: React.VFC<Props> = ({ collection, index }) => {
   const linkLabel = `${collection.name} collection`;
 
   return (
-    <article className="p-4 w-full md:py-6 lg:w-1/2">
-      <div className="overflow-hidden rounded-md shadow-md sm:flex">
-        <div className="h-56 overflow-hidden relative sm:w-1/2 sm:h-60">
-          <Link href={link}>
-            <a aria-label={linkLabel}>
-              <Image
-                src={collection.headerImage.url}
-                alt={collection.headerImage.alternativeText}
-                layout="fill"
-                objectFit="cover"
-              />
-            </a>
-          </Link>
-        </div>
-        <div className="h-60 p-4 relative sm:w-1/2">
-          <span className="inline-block -mt-6 text-8xl text-gray-200 sm:-mt-4">
-            #{pad(index, 2)}
-          </span>
-          <h2
-            className="-mt-5 text-4xl font-medium font-display text-gray-500 truncate md:-mt-6 lg:text-4xl"
-            title={collection.name}
+    <article className="rounded-md shadow-md lg:flex">
+      <div className="h-56 overflow-hidden relative sm:h-60 lg:w-1/2">
+        <Link href={link}>
+          <a aria-label={linkLabel}>
+            <Image
+              src={collection.headerImage.url}
+              alt={collection.headerImage.alternativeText}
+              layout="fill"
+              objectFit="cover"
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="h-60 p-4 relative lg:w-1/2">
+        <span className="inline-block -mt-6 text-8xl text-gray-200 sm:-mt-4">
+          #{pad(index, 2)}
+        </span>
+        <h2
+          className="-mt-5 text-4xl font-medium font-display text-gray-500 truncate md:-mt-6 lg:text-4xl"
+          title={collection.name}
+        >
+          {collection.name}
+        </h2>
+        <p className="mt-2 text-sm line-clamp-4 text-gray-400 sm:line-clamp-3 lg:text-base">
+          {collection.description}
+        </p>
+        <Link href={link}>
+          <a
+            target="_blank"
+            className="absolute right-4 bottom-4"
+            aria-label={linkLabel}
           >
-            {collection.name}
-          </h2>
-          <p className="mt-2 text-sm line-clamp-4 text-gray-400 sm:line-clamp-3 lg:text-base">
-            {collection.description}
-          </p>
-          <Link href={link}>
-            <a
-              target="_blank"
-              className="absolute right-4 bottom-4"
-              aria-label={linkLabel}
-            >
-              <IconExternalLink />
-            </a>
-          </Link>
-        </div>
+            <IconExternalLink />
+          </a>
+        </Link>
       </div>
     </article>
   );
--- components/collections/Collections.tsx
+++ components/collections/Collections.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 const Collections: React.VFC<Props> = ({ collections }) => {
   return (
-    <div className="md:flex md:flex-wrap md:justify-between">
+    <div className="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2">
       {collections.map((colection, i) => (
         <Collection collection={colection} index={i + 1} key={colection.id} />
       ))}
