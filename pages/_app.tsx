6700923c99064f92450cbf5aa8d7bae78420b44a
KarimLa <karimacc001@gmail.com>
2021-08-01 14:03:45 +0100

    removed apollo provider from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import { AppProps } from 'next/app';
 import { Router } from 'next/router';
 import Head from 'next/head';
-import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 
 import '../styles/globals.css';
 import '../styles/nprogress.css';
-import { useApollo } from '@/lib/apolloClient';
 import Page from '@/components/layout/Page';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -14,17 +12,13 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps);
-
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
+        <Component {...pageProps} />
       </Page>
     </>
   );
